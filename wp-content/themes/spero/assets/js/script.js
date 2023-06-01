a2ca09a70a5937800279497e9d0cb943c815fca8
Mohammad Luqman <luqman.geeky@gmail.com>
2023-06-01 13:01:04 +0530

    migrated to new site

---
+++ wp-content/themes/spero/assets/js/script.js
@@ -0,0 +1,22 @@
+(function($) {
+	
+	"use strict";
+	
+	//Tabs Box
+	if($('.tabs-box').length){
+		$('.tabs-box .tab-buttons .tab-btn').on('click', function(e) {
+			e.preventDefault();
+			var target = $($(this).attr('data-tab'));
+			
+			target.parents('.tabs-box').find('.tab-buttons').find('.tab-btn').removeClass('active-btn');
+				$(this).addClass('active-btn');
+				target.parents('.tabs-box').find('.tabs-content').find('.tab').fadeOut(0);
+				target.parents('.tabs-box').find('.tabs-content').find('.tab').removeClass('active-tab');
+				$(target).fadeIn(100);
+				$(target).addClass('active-tab');
+		});
+	}
+
+	
+
+})(window.jQuery);
